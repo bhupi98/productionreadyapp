ab12c5c5c6dae8b4c0b8ea9a6ade2a1432f810aa
bhupender <bhupender.cool98@gmail.com>
2023-03-21 18:31:43 +0530

    with parcel

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "./App";
+import ReactDOM from "react-dom/client";
+const heading = React.createElement(
+  "h1",
+  {},
+  "Hello Bhupi finally it is done and what about "
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
